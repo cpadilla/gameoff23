44a0850b28981e9f89d36d854f85bab8083c9e9b
Christofer Padilla <christoferpadilla@gmail.com>
2023-11-03 19:30:28 -0400

    Added main menu example to game. Split up code into files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use bevy::prelude::*;
+use plugin::*;
+
+mod plugin;
 
 fn main() {
     App::new()
-        .add_systems(Update, hello_world)
+        .add_plugins(DefaultPlugins)
+        // Insert as resource the initial value for the settings resources
+        .insert_resource(ClearColor(BLACK))
+        .insert_resource(DisplayQuality::Medium)
+        .insert_resource(Volume(7))
+        // Declare the game state, whose starting value is determined by the `Default` trait
+        .add_state::<GameState>()
+        .add_systems(Startup, setup)
+        // Adds the plugins for each state
+        .add_plugins((splash::SplashPlugin, menu::MenuPlugin, game::GamePlugin))
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
+
